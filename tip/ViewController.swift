3fef11eb7ba94aac45b7a5695c8a3a388a9c235e
monicayao <myao@g.hmc.edu>
2020-08-29 15:51:49 -0700

    adding additional features :)

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -14,30 +14,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
+    @IBOutlet weak var customTip: UISwitch!
+    @IBOutlet weak var customTipTextField: UITextField!
+    @IBOutlet weak var customTipLabel: UILabel!
+    let defaults = UserDefaults.standard
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // set initial state of all the parts
+        customTip.setOn(false, animated: false)
+        onCustomTipTap(self)
+        calculateTip(self)
+        billAmountTextField.becomeFirstResponder()
     }
 
+    @IBAction func onCustomTipTap(_ sender: Any) {
+        if (customTip.isOn){
+            customTipTextField.isHidden = false
+            customTipLabel.isHidden = false
+            tipControl.isHidden = true
+        }else{
+            customTipTextField.isHidden = true
+            customTipLabel.isHidden = true
+            tipControl.isHidden = false
+        }
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        // Number formatter for the fields
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
         
         // Get bill amount
-        let bill = Double(billAmountTextField.text!) ?? 0
-        
+        let bill = Double(billAmountTextField.text!) ?? 0.0
+        let customTipValue = Double(customTipTextField.text!) ?? 0.0
         // Calculate tip and total
-        let tipPercentages = [0.15, 0.18, 0.2]
-        
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
+        var tip = 0.0
+        var total = 0.0
+        if (customTip.isOn){
+            tip = bill * customTipValue / 100
+            total = bill + tip
+        }else{
+            let tipPercentages = [0.15, 0.18, 0.2]
+            tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+            total = bill + tip
+        }
         // Update tip and total labels
-        tipPercentageLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipPercentageLabel.text = currencyFormatter.string(from: NSNumber(value:tip))! //String(format: "$%.2f", tip)
+        totalLabel.text = currencyFormatter.string(from: NSNumber(value:total))! //String(format: "$%.2f", total)
     }
 
 }
